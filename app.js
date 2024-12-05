e45329e3ea8773891021cb2245fca1c9dfbbc7ee
rodolpho.toppan <rodolpho.toppan@db1.com.br>
2024-12-05 09:08:26 -0300

    fix: melhorias sugeridas no codigo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+const numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,40 +17,39 @@ function exibirMensagemInicial() {
 exibirMensagemInicial();
 
 function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log (chute == numeroSecreto);
+    const chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1','Parabéns!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+
+        const palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        const mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+
         exibirTextoNaTela('p', mensagemTentativas);
+        
         document.getElementById('reiniciar').removeAttribute('disabled');
-    }   else {
-        if(chute > numeroSecreto) {
+    }   else if (chute > numeroSecreto) {
             exibirTextoNaTela('p','O número secreto é menor!');
         } else {
             exibirTextoNaTela('p','O número secreto é maior!');
         }
+        
         tentativas++;
         limparCampo();
-    }
 }
 
 function gerarNumeroAleatorio () {
    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
-if(quantidadeDeElementosNaLista == numeroLimite) {
+if (quantidadeDeElementosNaLista == numeroLimite) {
     listaDeNumerosSorteados = [];
-
 }
 
    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio();
    } else {
     listaDeNumerosSorteados.push(numeroEscolhido);
-    console.log(listaDeNumerosSorteados);
     return numeroEscolhido;
    }
 }
